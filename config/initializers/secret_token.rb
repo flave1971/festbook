76593f0db3256506ee8621dbafe727cf222422b3
David Hill <dave.hill@hotmail.co.uk>
2013-08-29 15:06:13 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Festbook::Application.config.secret_token = '10264135dcca8074d3cf2fb11d84ba81ebcf227836523248a99255a9998a8e50126ae285db933b196578b4fd8b3db0f47a2ed724a5dc157797f87123b5d835da'
