4c9907f0a011d1cd9bbeb4faf0dd314ce877580c
David Hill <dave.hill@hotmail.co.uk>
2013-09-06 18:19:09 +0100

    updated flash messages, added helper method

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter their first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter their last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter their profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+  	user = User.new
+  	assert !user.save
+  	puts user.errors.inspect
+  	assert !user.errors[:profile_name].empty?
+  end
+
+
 end
